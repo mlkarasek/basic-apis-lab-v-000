50d59ea6004574756ae983aacece1e4ebc705671
Monica  Karasek <mlkarasek@gmail.com>
2018-10-10 10:12:09 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,7 +8,7 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = " Iv1.d3c9c182a048dfd6"
       req.params['client_secret'] = "4f68a1507b2635aec7df4f09db56c18cdf9d7096"
-      req.params['q'] = "tetris"
+      req.params['q'] = "query"
 
     end
     body = JSON.parse(@resp.body)
